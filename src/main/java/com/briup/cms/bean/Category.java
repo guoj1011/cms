563d0969844aa98dbaaa700f9e06bad9536f1b1a
guoj <1940485637@qq.com>
2019-11-28 19:21:04 +0800

    添加信息新增功能

--- src/main/java/com/briup/cms/bean/Category.java
+++ src/main/java/com/briup/cms/bean/Category.java
@@ -13,9 +13,9 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @ApiModelProperty(value = "栏目id")
     private int id;
-    @ApiModelProperty(value = "栏目编码",required = true)
+    @ApiModelProperty(value = "栏目编码")
     private long code;
-    @ApiModelProperty(value = "栏目名称",required = true)
+    @ApiModelProperty(value = "栏目名称")
     private String name;
 
     public int getId() {
